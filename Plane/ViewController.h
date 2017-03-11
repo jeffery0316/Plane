fdaec6e389dab86bc1b5953c6aa9e399de46ac45
JefferyKao <jeffery1720@gmail.com>
2017-03-11 22:07:40 +0800

    Initial Commit

---
+++ Plane/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Plane
+//
+//  Created by jefferykao on 2017/3/11.
+//  Copyright © 2017年 bike. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Plane/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Plane
+//
+//  Created by jefferykao on 2017/3/11.
+//  Copyright © 2017年 bike. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
